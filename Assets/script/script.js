327a79eb6697a20147548a4f8c6413d369f677b5
Meline Sarkissian <sarkissian.mell321@yahoo.com>
2023-09-19 22:14:08 -0700

    readme

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,24 +1,44 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var currentHour = dayjs().hour()
+console.log(currentHour)
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY h:mm A"))
+
+  $(".saveBtn").on("click", function () {
+
+    var userData = $(this).siblings(".description").val()
+    var timeBlock = $(this).parent().attr("id").split("-")[1]
+
+    console.log(userData, timeBlock)
+    localStorage.setItem(timeBlock, userData)
+  })
+
+  for (let i = 9; i <= 17; i++) {
+    var savedValue = localStorage.getItem(i)
+    $("#hour-" + i).children(".description").val(savedValue)
+    if (i < currentHour) {
+      $("#hour-" + i).children(".description").addClass("past")
+    } else if (i == currentHour) {
+      $("#hour-" + i).children(".description").addClass("present")
+    } else {
+      $("#hour-" + i).children(".description").addClass("future")
+    }
+  }
 });
+
+
+
+$(".saveBtn").on("click", function () {
+  var userData = $(this).siblings(".description").val()
+  var timeBlock = $(this).parent().attr("id").split("-")[1]
+  console.log(userData, timeBlock)
+  localStorage.setItem(timeBlock, userData)
+});
+
+$('.printBtn').click(function () {
+  window.print();
+  alert("Your schedule is now being printed and refreshed.");
+  localStorage.clear();
+});
+
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # daily-planner
+This is a work-day planner to help you schedule workday hours from 9AM to 5PM. Users can input tasks and save their tasks on the planner and it will color-code their tasks as past, present, and future. Past tasks will show in orange, present tasks in blue/green, and future tasks will be purple. 
+In addition, the current day and time is displayed on top of the page. 
+
+Here is a link to the deployed page: 
+
+Here is a screenshopt of how the page looks when opened:
+
